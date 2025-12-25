3b9192b8386ce47dde692a0f182e565b349ac171
Emeka Aziagba <emeka.aziagba@gmail.com>
2025-12-25 22:00:57 +0100

    changed database prefix from postgresql to postgresql+psycopg

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     # DATABASE_URL: str = "sqlite:///./todos.db"
     # postgresql://USERNAME:PASSWORD@HOST:PORT/DATABASE_NAME
     # DATABASE_URL: str = 'postgresql+psycopg://postgres:00000@localhost:5432/Todo'
-    DATABASE_URL: str = 'postgresql://mogdemdaz:nhvRY685R1WKZs5wRxRrKS6iN2X8Aw9z@dpg-d56pva3uibrs739p2ms0-a/todo_nmal'
+    DATABASE_URL: str = 'postgresql+psycopg://mogdemdaz:nhvRY685R1WKZs5wRxRrKS6iN2X8Aw9z@dpg-d56pva3uibrs739p2ms0-a/todo_nmal'
 settings = Settings()
 
 
