2437f080fb0b5f0d23af27da54d706910f4d79eb
Emeka Aziagba <emeka.aziagba@gmail.com>
2025-12-25 21:57:34 +0100

    changed the database string to match that of render platform

--- app/core/config.py
+++ app/core/config.py
@@ -2,19 +2,18 @@ import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-# class Settings(BaseSettings):
+class Settings(BaseSettings):
     # DATABASE_URL: str = "sqlite:///./todos.db"
     # postgresql://USERNAME:PASSWORD@HOST:PORT/DATABASE_NAME
-    # DATABASE_URL: str
     # DATABASE_URL: str = 'postgresql+psycopg://postgres:00000@localhost:5432/Todo'
-
-# settings = Settings()
-
-
-class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv(
-        "DATABASE_URL",
-        "postgresql+psycopg://postgres:00000@localhost:5432/Todo"
-    )
-
+    DATABASE_URL: str = 'postgresql://mogdemdaz:nhvRY685R1WKZs5wRxRrKS6iN2X8Aw9z@dpg-d56pva3uibrs739p2ms0-a/todo_nmal'
 settings = Settings()
+
+
+# class Settings(BaseSettings):
+#     DATABASE_URL: str = os.getenv(
+#         "DATABASE_URL",
+#         "postgresql+psycopg://postgres:00000@localhost:5432/Todo"
+#     )
+#
+# settings = Settings()
