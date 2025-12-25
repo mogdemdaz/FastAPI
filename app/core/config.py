5e2c30d5f3beefa1ff6be5639b2dbf3c71795a10
Emeka Aziagba <emeka.aziagba@gmail.com>
2025-12-25 21:41:51 +0100

    added environment virables

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,20 @@
-from pydantic_settings import BaseSettings
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+# class Settings(BaseSettings):
+    # DATABASE_URL: str = "sqlite:///./todos.db"
+    # postgresql://USERNAME:PASSWORD@HOST:PORT/DATABASE_NAME
+    # DATABASE_URL: str
+    # DATABASE_URL: str = 'postgresql+psycopg://postgres:00000@localhost:5432/Todo'
+
+# settings = Settings()
 
 
 class Settings(BaseSettings):
-    # DATABASE_URL: str = "sqlite:///./todos.db"
-    # postgresql://USERNAME:PASSWORD@HOST:PORT/DATABASE_NAME
-
-    DATABASE_URL: str = 'postgresql+psycopg://postgres:00000@localhost:5432/Todo'
+    DATABASE_URL: str = os.getenv(
+        "DATABASE_URL",
+        "postgresql+psycopg://postgres:00000@localhost:5432/Todo"
+    )
 
 settings = Settings()
